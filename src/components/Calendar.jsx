88104d2e1b33be10d0cb595bb4f2f5346bafddaf
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-05-13 18:59:31 +0100

    calendar added

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+const Calendar = () => {
+  const [date, setDate] = useState(new Date());
+  return (
+    <DatePicker
+    showIcon
+      selected={date}
+      onChange={(date) => setDate(date)}
+      showTimeSelect
+      timeIntervals={15}
+      dateFormat="dd/MM/yyyy h:mm aa"
+    />
+  );
+};
+
+export default Calendar;
--- src/components/RecordRun.jsx
+++ src/components/RecordRun.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react"
+import { useState } from "react";
+import Calendar from "./Calendar";
 
 const RecordRun = () => {
-
-    const [distance, setDistance] = useState('')
-    const [time, setTime] = useState('')
-
-
-
+  const [distance, setDistance] = useState("");
+  const [time, setTime] = useState("");
 
   return (
     <>
-    <p>RecordRun</p>
-    <h2>Select a Date</h2>
-
+      <p>RecordRun</p>
+      <h2>Select a Date</h2>
+      <Calendar />
     </>
-  )
-}
+  );
+};
 
-export default RecordRun
+export default RecordRun;
